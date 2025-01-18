a308cb8bc2202606a986f711b4474d6ba83815cc
web3sandyyy <sandeeprajapati18@gmail.com>
2025-01-18 15:31:10 +0530

    home page added

---
+++ client-side/src/pages/Home.tsx
@@ -0,0 +1,16 @@
+const Home = () => {
+  return (
+    <div className="min-h-[100dvh] w-full bg-white">
+      <div className="w-full">
+        <p>Aspire AI </p>
+
+        <div>
+          <p>Sign In</p>
+          <p>Register</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
