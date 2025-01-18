551c4c30ffd9ba52e93b1c4bd47badd4f512fddf
Mehek Jain <mehekjain28@gmail.com>
2025-01-18 16:40:30 +0530

    home page

--- client-side/src/main.tsx
+++ client-side/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import './index.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import * as ReactDOM from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <ChakraProvider>
+    {/* <StrictMode> */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    {/* </StrictMode> */}
+  </ChakraProvider>
+  ,
 )
