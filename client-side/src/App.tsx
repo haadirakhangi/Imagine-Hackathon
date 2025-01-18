bdf45b40811c7af79a060cce20bf56e7bb74dd60
Mehek Jain <mehekjain28@gmail.com>
2025-01-18 16:16:13 +0530

    version changed

--- client-side/src/App.tsx
+++ client-side/src/App.tsx
@@ -1,10 +1,17 @@
 import "./App.css";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import { Route, Routes, Navigate } from "react-router-dom";
 
 function App() {
   return (
+    
     <div className="min-h-[100dvh] w-full bg-white">
-      <Home />
+      <Routes>
+      <Route path="*" element={<Navigate to="/" />} />
+      <Route element={<Home />} path="/" />
+      <Route element={<Login />} path="/login" />
+      </Routes>
     </div>
   );
 }
